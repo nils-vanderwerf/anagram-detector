aaaeebc5bbb5b24b579fd17f367ea5765404790a
Nils van der Werf <n.vanderw.92@gmail.com>
2022-03-11 16:25:02 +1100

    makes anagrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :source_word
+  def initialize(word)
+    @source_word = word
+  end
+
+  def match(array_to_compare)
+    #select leaves empty array if there's no return value
+    array_to_compare.select {|x| x.split("").sort == @source_word.split("").sort }
+  end
+end
+
+listen = Anagram.new("listen")
+listen.match(%w(enlists google inlets banana))
